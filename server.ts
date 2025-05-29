dcfbf9daac198aca228b399187cc490ab658c982
cafread <colin.read@gmail.com>
2025-05-29 17:13:45 +0100

    Change cron frequency now testing is complete

--- server.ts
+++ server.ts
@@ -59,7 +59,7 @@ Deno.serve({port: servePort, hostname: servIP}, (request: Request) => {
     }
 });
 
-Deno.cron("Deletions and KV miss check", {minute: {every: 10}}, () => {
+Deno.cron("Deletions and KV miss check", {hour: {every: 1}}, () => {
     // Run through any scheduled deletions, not relying on onStart to find them
     processPendingDeletions().catch((err) => console.error("Error in periodic deletion handler:", err));
     // Check that kv holds all tile keys and queue tile read where this misses
